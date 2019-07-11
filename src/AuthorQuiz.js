2563224ffe83f3f6d90c9c24d8f8b5297dfd0805
Jorge Candeias <candeias@fastmail.fm>
2019-07-11 18:05:49 +0100

    stuff

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      Author Quiz
+    </div>
+  );
+}
+
+export default App;
