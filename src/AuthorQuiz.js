a306c8bd3e9f0fd96b9c4a787452133434f7f389
Jorge Candeias <candeias@fastmail.fm>
2019-07-16 00:27:09 +0100

    Added authors

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -3,12 +3,56 @@ import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
 
-function AuthorQuiz() {
+function Hero() {
+  return (
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>What book did this author write?</p>
+      </div>
+    </div>);
+}
+
+function Book({ title }) {
+  return (
+    <div className="answer">
+      <h4>{title}</h4>
+    </div>
+  )
+}
+
+function Turn({ author, books }) {
+  return (
+    <div className="row turn" style={{ backgroundColor: "white" }}>
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="authorimage" alt="Author" />
+      </div>
+      <div className="col-6">
+        {books.map((title) => <Book title={title} key={title} />)}
+      </div>
+    </div>
+  );
+}
+
+function Continue() {
+  return (null);
+}
+
+function Footer() {
+  return (
+    <div className="col-12">
+      <p>All images are from <a href="https://commons.wikimedia.org/wiki/Main_Page">Wikimedia Commons</a> and are in the public domain.</p>
+    </div>
+  );
+}
+
+function AuthorQuiz({ turnData }) {
   return (
     <div className="container-fluid">
       <Hero />
-      <Turn />
+      <Turn {...turnData} />
       <Continue />
+      <Footer />
     </div>
   );
 }
